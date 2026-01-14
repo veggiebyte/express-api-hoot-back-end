c09d71578fcb2554b0f638dc26ec5ffa35cae3c5
“CindyS” <veggiebyte@yahoo.com>
2026-01-14 10:36:05 -0800

    init commit

---
+++ controllers/auth.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const saltRounds = 12;
+
+router.post('/sign-up', async (req, res) => {
+  try {
+    // Check if the username is already taken
+    const userInDatabase = await User.findOne({ username: req.body.username });
+
+    if (userInDatabase) {
+      return res.status(409).json({ err: 'Username already taken.' });
+    }
+
+    // Create a new user with hashed password
+    const user = await User.create({
+      username: req.body.username,
+      hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
+    });
+
+    // Construct the payload
+    const payload = { username: user.username, _id: user._id };
+    // Create the token, attaching the payload
+    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
+    // Send the token instead of the user
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+});
+
+router.post('/sign-in', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json({ err: 'Invalid credentials.' });
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password, user.hashedPassword
+    );
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ err: 'Invalid credentials.' });
+    }
+
+    // Construct the payload
+    const payload = { username: user.username, _id: user._id };
+
+    // Create the token, attaching the payload
+    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
+
+    // Send the token instead of the message
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// ... other requires above
+const testJwtRouter = require('./controllers/test-jwt');
+const authRouter = require('./controllers/auth');
+const usersRouter = require('./controllers/users');
+
+// ... other middleware
+
+// Routes go here
+app.use('/auth', authRouter);
+app.use('/test-jwt', testJwtRouter);
+app.use('/users', usersRouter);
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
