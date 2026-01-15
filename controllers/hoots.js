127edb59d7056d8254503c3696464eb2451cee80
“CindyS” <veggiebyte@yahoo.com>
2026-01-15 00:24:49 -0800

    Create Comment

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -77,4 +77,24 @@ router.delete("/:hootId", async (req, res) => {
   }
 });
 
+router.post("/:hootId/comments", verifyToken, async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.findById(req.params.hootId);
+    hoot.comments.push(req.body);
+    await hoot.save();
+
+    // Find the newly created comment:
+    const newComment = hoot.comments[hoot.comments.length - 1];
+
+    newComment._doc.author = req.user;
+
+    // Respond with the newComment:
+    res.status(201).json(newComment);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+
 module.exports = router;
