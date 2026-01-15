d176b8b1290dbf957c3e6ee47585cfc1f58252d2
“CindyS” <veggiebyte@yahoo.com>
2026-01-15 00:44:47 -0800

    Delete Comment

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -122,5 +122,27 @@ router.put("/:hootId/comments/:commentId", verifyToken, async (req, res) => {
 });
 
 
+router.delete("/:hootId/comments/:commentId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    const comment = hoot.comments.id(req.params.commentId);
+
+    // ensures the current user is the author of the comment
+    if (comment.author.toString() !== req.user._id) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to edit this comment" });
+    }
+
+    hoot.comments.remove({ _id: req.params.commentId });
+    await hoot.save();
+    res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+
+
 
 module.exports = router;
